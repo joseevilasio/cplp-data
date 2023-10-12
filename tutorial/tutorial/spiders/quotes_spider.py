ee7997338e14e39ba9418c2b2136e66f1ebdc66e
joseevilasio <jose_evilasio@outlook.com>
2023-10-12 12:35:45 +0100

    utilizando response follow

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -10,7 +10,7 @@ class QuotesSpider(scrapy.Spider):
         urls = [
             "https://quotes.toscrape.com/page/1/",
         ]
-        
+
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
@@ -24,5 +24,4 @@ class QuotesSpider(scrapy.Spider):
 
         next_page = response.css("li.next a::attr(href)").get()
         if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+            yield response.follow(next_page, callback=self.parse)
