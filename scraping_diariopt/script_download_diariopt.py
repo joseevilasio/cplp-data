2cea124dc754f41920d0796181dbe1d3f823a406
joseevilasio <jose_evilasio@outlook.com>
2023-10-26 15:45:19 +0100

    function download pdf and script

--- scraping_diariopt/script_download_diariopt.py
+++ scraping_diariopt/script_download_diariopt.py
@@ -1,38 +1,68 @@
-from urllib import request
+import urllib3
 from datetime import datetime
 import os
 import pandas as pd
 from time import sleep
 
 
+def download_pdf(url: str, path: str) -> bool:
+    """Receive a pdf file link and download"""
+
+    http = urllib3.PoolManager()
+
+    response = http.request("GET", url, preload_content=False)
+    content_type = response.headers["content-type"]
+
+    print(f"Get -> {url}\n")
+
+    sleep(2)
+
+    if content_type == "application/pdf":
+        with open(path, "wb") as file:
+            for chunk in response.stream(1024):
+                file.write(chunk)
+
+    else:
+        print("Fail download")
+        return False
+
+
+    response.release_conn()
+    print(f"Download Done -> {path_pdf}")
+    return True
+
+
 # Path
 here = os.path.dirname(os.path.abspath(__file__))
 raw_path = "".join([here, "/assets/raw_pdf"])
 
-data = {        
+data_download = {        
     "date_save_pdf": [], # Date when save pdf
     "pdf_ok": [], # If the pdf has been saved (True or False)
 }
 
-spreedsheet = pd.read_csv("".join([raw_path, "/data_scraping_raw.csv"]), sep=";")
+data_links = pd.read_csv("".join([raw_path, "/data_scraping_raw.csv"]), sep=";")
 
-for index in range(len(spreedsheet)):
+for index in range(len(data_links)):
 
-    link_pdf = spreedsheet.link_pdf[index]
-    name_pdf = spreedsheet.name_pdf[index]
+    link_pdf = data_links.link_pdf[index]
+    name_pdf = data_links.name_pdf[index]
+    path_pdf = "".join([raw_path, f"/{name_pdf}"])
 
-    if os.path.exists("".join([raw_path, f"/{name_pdf}"])):
+    if os.path.exists(path_pdf):
         result = True
+
     else:
-        print(f"Download - {link_pdf} -- Arquivo - {name_pdf}")    
-        response = True # request.urlretrieve(link_pdf, "".join([raw_path, f"/{name_pdf}"]))
-        # result = True if response else result = False
+        result = download_pdf(url=link_pdf, path=path_pdf)    
+    
+    data_download["date_save_pdf"].append(datetime.now().strftime("%d/%m/%Y %H:%M"))
+    
+    data_download["pdf_ok"].append(result)
 
-    sleep(2)    
 
-    data["date_save_pdf"] = datetime.now()
-    data["pdf_ok"] = result
+data_download = pd.DataFrame(data_download)
 
-print(data)
+# Concat data download and data infor links
+data_complete = pd.concat([data_links, data_download], axis=1)
 
-#TODO: Mesclar dados com a planilha
+print(data_complete.head())
--- scraping_diariopt/script_scraping_diariopt.py
+++ scraping_diariopt/script_scraping_diariopt.py
@@ -1,4 +1,4 @@
-from selenium import webdriver  
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
 import os
@@ -23,68 +23,82 @@ sleep(3)
 
 # Search
 text_for_research = "Concede o estatuto de igualdade de direitos e deveres a vários cidadãos brasileiros"
-input_place = browser.find_element(By.TAG_NAME, "input") # Find the search box
-input_place.send_keys(text_for_research) # Insert text
+input_place = browser.find_element(By.TAG_NAME, "input")  # Find the search box
+input_place.send_keys(text_for_research)  # Insert text
 
-buttom_search = browser.find_element(By.ID, "b2-b2-myButton2") # Find the buttom submit
-buttom_search.click() # Submit
+buttom_search = browser.find_element(By.ID, "b2-b2-myButton2")  # Find the buttom submit
+buttom_search.click()  # Submit
 
 sleep(3)
 
 # Search filter
-checkbox_legislacao = browser.find_elements(By.CLASS_NAME, "checkbox") # Find the checkbox 'Legislação'
-checkbox_legislacao[1].click() # Check
+checkbox_legislacao = browser.find_elements(
+    By.CLASS_NAME, "checkbox"
+)  # Find the checkbox 'Legislação'
+checkbox_legislacao[1].click()  # Check
 
 sleep(3)
 
-checkbox_serie_plus = browser.find_element(By.XPATH, "//*[@id='Serie_Titulo']/div[1]/span") # Find area to expand option 'Série'
+checkbox_serie_plus = browser.find_element(
+    By.XPATH, "//*[@id='Serie_Titulo']/div[1]/span"
+)  # Find area to expand option 'Série'
 checkbox_serie_plus.click()
 
 sleep(2)
 
-checkbox_serie = browser.find_elements(By.CLASS_NAME, "checkbox") # Find the checkbox 'Série I'
-checkbox_serie[4].click() # Check
+checkbox_serie = browser.find_elements(
+    By.CLASS_NAME, "checkbox"
+)  # Find the checkbox 'Série I'
+checkbox_serie[4].click()  # Check
 
 sleep(2)
 
-#TODO: Configurar para ter 200 resultados na pagina e avançar para próxima paginas
-#TODO: Filtar datas
-#TODO: Ordenar resultado de pesquisa
+# TODO: Configurar para ter 200 resultados na pagina e avançar para próxima paginas
+# TODO: Filtar datas
+# TODO: Ordenar resultado de pesquisa
 
 
 # Data extraction
-body_results = browser.find_element(By.ID, "ListaResultados") # Find data
-list_href_page = body_results.find_elements(By.CLASS_NAME, "title") # Find element in data (create list)
+body_results = browser.find_element(By.ID, "ListaResultados")  # Find data
+list_href_page = body_results.find_elements(
+    By.CLASS_NAME, "title"
+)  # Find element in data (create list)
 
-data = {        
-    "description": [], #description
-    "link_page": [], #link page 'despacho'
-    "link_pdf": [], #link page file download
-    "name_pdf": [] #name pdf file
+data = {
+    "description": [],  # description
+    "link_page": [],  # link page 'despacho'
+    "link_pdf": [],  # link page file download
+    "name_pdf": [],  # name pdf file
 }
 
 for item_href in list_href_page:
-
-    link_page = item_href.get_attribute("href") # Link for page 'despacho'
-    text_page = item_href.find_element(By.CSS_SELECTOR, "span").text # Extraction text 'despacho'
+    link_page = item_href.get_attribute("href")  # Link for page 'despacho'
+    text_page = item_href.find_element(
+        By.CSS_SELECTOR, "span"
+    ).text  # Extraction text 'despacho'
 
     data["description"].append(text_page)
     data["link_page"].append(link_page)
 
 for link in data["link_page"]:
-
     # Get link page 'despacho'
     browser.get(f"{link}")
 
     sleep(3)
 
-    list_elements_page_download = browser.find_elements(By.CLASS_NAME, "ThemeGrid_MarginGutter") # List of elements in page
-    download_link_pdf = list_elements_page_download[-1].get_attribute("href") # Extraction link file pdf
-    parser_name_pdf = download_link_pdf.split("/")[-1] # Parser link file pdf to pdf name
+    list_elements_page_download = browser.find_elements(
+        By.CLASS_NAME, "ThemeGrid_MarginGutter"
+    )  # List of elements in page
+    download_link_pdf = list_elements_page_download[-1].get_attribute(
+        "href"
+    )  # Extraction link file pdf
+    parser_name_pdf = download_link_pdf.split("/")[
+        -1
+    ]  # Parser link file pdf to pdf name
 
     data["link_pdf"].append(download_link_pdf)
     data["name_pdf"].append(parser_name_pdf)
- 
+
 # Close webdriver
 browser.quit()
 
