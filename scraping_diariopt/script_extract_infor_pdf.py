0f4d6280c91ea3c4a09323f920752e7b8cd7a986
joseevilasio <jose_evilasio@outlook.com>
2023-10-30 16:55:03 +0000

    extract infor pdf first steps

--- scraping_diariopt/script_extract_infor_pdf.py
+++ scraping_diariopt/script_extract_infor_pdf.py
@@ -1,6 +1,34 @@
 from PyPDF2 import PdfReader
+import re
 
+# TODO: Fazer um for com arquivos pdf
 
 reader = PdfReader("scraping_diariopt/assets/raw_pdf/0004400044.pdf")
-page = reader.pages[0]
-print(page.extract_text())
+page = reader.pages[0] # Pode haver mais de uma pagina
+num_pages = len(reader.pages)
+
+
+text = page.extract_text().replace("\n", "").replace(" .", "") #retirar quebra de linhas e retirar "."
+texto_para_ponto_inicial = "nascimento"
+tam_texto_para_ponto_inicial = len(texto_para_ponto_inicial)
+indice_inicial = text.find(texto_para_ponto_inicial) + tam_texto_para_ponto_inicial
+
+extract = text[indice_inicial:]
+
+date = r"(\d{2}/\d{2}/\d{4})"
+name = r"^[a-zA-ZÀ-ÖØ-öø-ÿ\s]+$"
+
+list_objs_re = re.split(date, extract)
+
+data = {
+    "name": [],
+    "date_born": []
+}
+
+for item in list_objs_re:
+    if re.search(name, item):
+        data["name"].append(item.strip())
+    elif re.search(date, item):
+        data["date_born"].append(item)
+
+print(data)
