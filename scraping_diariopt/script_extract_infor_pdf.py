b22be6bc293c867b24f2f64642efb57b053c2f54
joseevilasio <jose_evilasio@outlook.com>
2023-11-01 00:06:36 +0000

    function done

--- scraping_diariopt/script_extract_infor_pdf.py
+++ scraping_diariopt/script_extract_infor_pdf.py
@@ -10,7 +10,7 @@ def extract_text_pdf(file) -> list:
     reader = PdfReader(file) # instance
     number_of_pages = len(reader.pages)
 
-    pattern_date = r"(\d{2}/\d{2}/\d{4})"
+    pattern_date = r"(\d{1,2}\s?\d{0,1}\s?/\s?\d{2}\s?/\s?\d{4})"
     pattern_name = r"^[a-zA-ZÀ-ÖØ-öø-ÿ\s]+$"
 
     name = []
@@ -20,7 +20,7 @@ def extract_text_pdf(file) -> list:
 
         page = reader.pages[number]
 
-        text = page.extract_text().replace("\n", "").replace(" .", "") # remove line break and symbol dot
+        text = page.extract_text().replace("\n", "").replace(".", "").replace("//", "/").replace("’", "") # remove line break and symbol dot
         word_initial_point = "nascimento" # starting point for cutting
 
         amount_word_initial_point = len(word_initial_point)
@@ -28,13 +28,17 @@ def extract_text_pdf(file) -> list:
 
         extract_infor = text[index_initial:] # information after index initial       
 
-        list_infor_split = re.split(pattern_date, extract_infor) # regex split with pattern date        
+        list_infor_split = re.split(pattern_date, extract_infor) # regex split with pattern date
 
+        print(f"{file}")
         for item in list_infor_split:
             if re.search(pattern_name, item):
                 name.append(item.strip())
+                print(f"Name: {item.strip()} --- ", end="")
             elif re.search(pattern_date, item):
-                birth_date.append(item)
+                birth_date.append(item.replace(" ", ""))
+                print(f"Date: {item.strip()}")
+            
 
     return name, birth_date
 
@@ -59,12 +63,35 @@ for index in range(len(data_scraping_complete)):
     name_pdf = data_scraping_complete.name_pdf[index]
     file_pdf = "".join([raw_path, f"/{name_pdf}"])
     exist_file = data_scraping_complete.pdf_ok[index]
-
-    data["description"].append(data_scraping_complete.description[index])
-    data["link_page"].append(data_scraping_complete.link_page[index])
+    description = data_scraping_complete.description[index]
+    link_page = data_scraping_complete.link_page[index]    
 
     if exist_file:
-        name, birth_date = extract_text_pdf(file=file_pdf)
+        list_name, list_birth_date = extract_text_pdf(file=file_pdf)
+        
+        [data["name"].append(name) for name in list_name]
+        [data["birth_date"].append(birth_date) for birth_date in list_birth_date]
+        [data["description"].append(description) for x in range(len(list_name))]
+        [data["link_page"].append(link_page) for x in range(len(list_name))]
+        [data["extract_complete"].append(True) for x in range(len(list_name))]
+
     else:
         print(f"File no exist -> {file_pdf}")
+        
+        data["extract_complete"].append(False)
+        data["name"].append(None)
+        data["birth_date"].append(None)
+        data["description"].append(description)
+        data["link_page"].append(link_page)
 
+
+data_extract = pd.DataFrame(data)
+data_extract.to_csv(
+    "".join([processed_data, "/processed_data_extract.csv"]), sep=";", index=False
+)
+
+# print(len(data["extract_complete"]))
+# print(len(data["birth_date"]))
+# print(len(data["description"]))
+# print(len(data["link_page"]))
+# print(len(data["name"]))
