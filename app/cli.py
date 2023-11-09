31983608fed73c1ab235c44583de92f331e71fae
joseevilasio <jose_evilasio@outlook.com>
2023-11-09 20:12:59 +0000

    add command 'report'

--- app/cli.py
+++ app/cli.py
@@ -1,64 +1,34 @@
 import typer
 from rich.console import Console
 from rich.table import Table
-
+import pandas as pd
 
 main = typer.Typer(name="CPLP DATA CLI", add_completion=False)
 
 
 @main.command()
 def start_web_scraping():
-    """Lists all users"""
-    table = Table(title="dundie users")
-    fields = ["name", "username", "dept", "email", "currency"]
-    for header in fields:
-        table.add_column(header, style="magenta")
-
-    with Session(engine) as session:
-        users = session.exec(select(User))
-        for user in users:
-            table.add_row(*[getattr(user, field) for field in fields])
-
-    Console().print(table)
+    pass
 
 
 @main.command()
-def start_download_pdf(
-    name: str,
-    email: str,
-    password: str,
-    dept: str,
-    username: str | None = None,
-    currency: str = "USD",
-):
-    """Create user"""
-    with Session(engine) as session:
-        user = User(
-            name=name,
-            email=email,
-            password=password,  # pyright: ignore
-            dept=dept,
-            username=username or generate_username(name),
-            currency=currency,
-        )
-        session.add(user)
-        session.commit()
-        session.refresh(user)
-        typer.echo(f"created {user.username} user")
-        return user
+def start_download_pdf():
+    pass
 
 
 @main.command()
 def report():
-    """Lists all users"""
-    # table = Table(title="dundie users")
-    # fields = ["name", "username", "dept", "email", "currency"]
-    # for header in fields:
-    #     table.add_column(header, style="magenta")
+    """Read file default with information about data"""
 
-    # with Session(engine) as session:
-    #     users = session.exec(select(User))
-    #     for user in users:
-    #         table.add_row(*[getattr(user, field) for field in fields])
+    table = Table(title="CPLP DATA - Report")
+    fields = ["Search Range", "Pages", "Downloaded", "Pages Extract", "Names Extract"]
+    for header in fields:
+        table.add_column(header, style="magenta")
 
-    # Console().print(table)
+    default = pd.read_csv("./app/log/default.csv", sep=";")
+    default_dict = default.to_dict(orient="records")
+
+    for row in default_dict:
+        table.add_row(*[str(value) for value in row.values()])    
+
+    Console().print(table)
